745286c35ac811a1c04bb6a2e9b2e9b5c59bfa1f
deeptuuk <zhouy2048@gmail.com>
2021-07-29 13:53:08 +0800

    add block chain

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,21 +1,10 @@
-use dp_blockchain::{self, Block};
+use dp_blockchain::{self, BlockChain};
 //use sha2;
 
 fn main() {
-    dp_blockchain::test_work();
-    println!("hello, world!");
+    let mut blockchain = BlockChain::new_blockchain();
+    blockchain.add_block(String::from("Send 1 BTC to Ivan"));
+    blockchain.add_block(String::from("Send 2 more BTC to Ivan"));
 
-    let first_block_data = String::from("This is a test data");
-    let first_block = Block::new(20210729, first_block_data, [0u8; 32]);
-
-    println!("{:?}", first_block)
-
-    // let mut block = Block {
-    //     timestamp: 20210728,
-    //     data: String::from("This is a test data"),
-    //     prev_block_hash: [0u8; 32],
-    //     hash: [0u8; 32],
-    // };
-
-    // block.set_hash();
+    blockchain.show_blockchain();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use sha2::{Sha256, Digest};
-use std::str;
 use std::convert::TryInto;
+use std::time::{SystemTime, Duration};
 
 #[cfg(test)]
 mod tests {
@@ -11,8 +11,8 @@ mod tests {
 }
 
 #[derive(Debug)]
-pub struct Block {
-    timestamp: u128,
+struct Block {
+    timestamp: Duration,
     data: String,
     prev_block_hash: [u8; 32],
     hash: [u8; 32],
@@ -21,10 +21,10 @@ pub struct Block {
 //sha2::digest::generic_array::GenericArray<u8, <Sha256 as Digest>::OutputSize>
 
 impl Block {
-    pub fn new(timestamp: u128, data: String, prev_block_hash: [u8; 32]) -> Block {
+    pub fn new_block(data: String, prev_block_hash: [u8; 32]) -> Block {
 
         let mut temp = Block {
-            timestamp: timestamp,
+            timestamp: SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap(),
             data: data,
             prev_block_hash: prev_block_hash,
             hash: [0u8; 32],
@@ -35,12 +35,40 @@ impl Block {
     }
 
 
-    pub fn set_hash(&mut self) {
+    fn set_hash(&mut self) {
         let mut hasher = Sha256::new();
-        hasher.update((self.timestamp.to_string() + &self.data + str::from_utf8(&self.prev_block_hash).unwrap()).as_bytes());
+        hasher.update( [self.timestamp.as_nanos().to_string().as_bytes(), self.data.as_bytes(), &self.prev_block_hash ].concat() );
         let result = hasher.finalize();
         self.hash = result.as_slice().try_into().expect("slice with incorrect length");
-        println!("{:?}", self.hash)
+    }
+}
+
+#[derive(Debug)]
+pub struct BlockChain {
+    blocks: Vec<Block>
+}
+
+impl BlockChain {
+    pub fn new_blockchain() -> BlockChain {
+        let genesis_block = Block::new_block(String::from("Genesis Block"), [0u8; 32]);
+        let mut blockchain =  BlockChain {
+            blocks: Vec::new(),
+        };
+        blockchain.blocks.push(genesis_block);
+        blockchain
+    }
+    pub fn add_block(&mut self, data: String) {
+        self.blocks.push(Block::new_block(data, self.blocks[self.blocks.len() - 1].hash));
+    }
+
+    pub fn show_blockchain(&self) {
+        for i in &self.blocks {
+            println!("TimeStamp: {:?}", i.timestamp);
+            println!("Prev_hash: {:02x?}", i.prev_block_hash);
+            println!("Data     : {}", i.data);
+            println!("Hash     : {:02x?}", i.hash);
+            println!("");
+        }
     }
 }
 

cd8868918983a0970352e8200fe10b14293bae68
deeptuuk <zhouy2048@gmail.com>
2021-08-05 18:06:11 +0800

    finish part 4

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,32 +1,26 @@
-use dp_blockchain::{Block, BlockChainDb, BlockchainIterator};
+use dp_blockchain4::{Block, BlockChainDb, BlockchainIterator, Transaction, Pool};
 use clap::{Arg, App, SubCommand};
 
 fn main() {
-    let mut blockchain = BlockChainDb::new_blockchain();
-    // //blockchain.add_block(String::from("Send 1 BTC to Ivan"));
-    // //blockchain.add_block(String::from("Send 2 more BTC to Ivan"));
-    // //blockchain.add_block(String::from("Send 1 BTC to Ivan"));
+    let mut blockchain = BlockChainDb::new_blockchain(String::from("Ivan"));
+    //blockchain.add_block(vec![Transaction::new_coinbase_tx(String::from("Ivan"))]);
+    // let mut pool = Pool::new_pool();
+    // pool.add_transaction_to_pool(&blockchain, "Ivan", "Test", 5);
+    //blockchain.add_block(vec![Transaction::new_coinbase_tx(String::from("Ivan"))]);
+    //println!("{:?}", blockchain.find_spendable_transaction_output_index(&pool, "Ivan", 60));
+    //println!("{:?}", blockchain.find_unspent_transactions(&pool, "Ivan"));
+    // println!("------------------------------------------------------");
 
-    // let mut blockchainiterator = BlockchainIterator::new(blockchain);
+    // blockchain.add_block(vec![Transaction::new_utxo_transaction(&blockchain, "Ivan", "Test", 5).unwrap()]);
+    // //blockchain.add_block(vec![Transaction::new_utxo_transaction(&blockchain, "Ivan", "Test", 5).unwrap()]);
+    //blockchain.show_blockchain();
 
-    // loop {
-    //     if let Some(block) = blockchainiterator.next() {
-    //         //println!("{:?}", block)
-    //         Block::show_block(&block);
-    //     }
-    //     else {
-    //         break;
-    //     }
-    // };
-    // //blockchain.add_block(String::from("Send 1 BTC to Ivan"));
-    // //blockchain.add_block(String::from("Send 2 more BTC to Ivan"));
+    // println!("{:?}", blockchain.find_unspent_transactions("Ivan"));
 
-    // //blockchain.show_blockchain();
+    //println!("------------------------------------------------------");
 
-    // // let x: &[u8] =&[1,2,3];
-    // // println!("{:?}", x);
-    // println!("hello world!");
-
+    //println!("{:?}", blockchain.find_unspent_transactions("Test"));
+    //println!("{:?}", blockchain.find_spendable_outputs("Ivan", 5));
     let matches = App::new("DP_BlockChain")
                           .version("1.0")
                           .author("Deep Tuuk. <zhouy2048@gmail.com>")
@@ -34,30 +28,53 @@ fn main() {
                           .arg(Arg::with_name("p")
                                .short("p")
                                .long("--print")
-                               .help("Print the Block Chain"))                
-                          .arg(Arg::with_name("a")
+                               .help("Print the Block Chain"))              
+                          .arg(Arg::with_name("b")
+                               .short("b")
+                               .long("--balance")
+                               .value_name("Address")
+                               .help("Print the Balance")
+                               .takes_value(true))    
+                          .arg(Arg::with_name("from")
+                               .short("f")
+                               .long("--from")
+                               .value_name("Address")
+                               .help("Send from")
+                               .takes_value(true))             
+                          .arg(Arg::with_name("to")
+                               .short("t")
+                               .long("--to")
+                               .value_name("Address")
+                               .help("Send to")
+                               .takes_value(true))              
+                          .arg(Arg::with_name("amount")
                                .short("a")
-                               .long("--add")
-                               .value_name("String")
-                               .help("Add a Block")
-                               .takes_value(true))                                         
+                               .long("--amount")
+                               .value_name("amount")
+                               .help("Send amount")
+                               .takes_value(true))                                                                                      
                           .get_matches();
 
     if matches.is_present("p") {
-        let mut blockchainiterator = BlockchainIterator::new(&blockchain);
-
-        loop {
-            if let Some(block) = blockchainiterator.next() {
-                //println!("{:?}", block)
-                Block::show_block(&block);
-            }
-            else {
-                break;
-            }
-        };
+        blockchain.show_blockchain();
     }
+    
+    if let Some(message) = matches.value_of("b") {
+        println!("Balance of '{}': {}", message, blockchain.get_balance(message));
+    }    
 
-    if let Some(message) = matches.value_of("a") {
-        blockchain.add_block(String::from(message));
+    if let Some(from) = matches.value_of("from") {
+        if let Some(to) = matches.value_of("to") {
+            if let Some(amount) = matches.value_of("amount") {
+
+                //blockchain.add_block(vec![Transaction::new_coinbase_tx(String::from("Ivan"))]);
+
+                let mut pool = Pool::new_pool();
+                pool.add_transaction_to_pool(&blockchain, from, to, amount.parse::<i64>().unwrap());
+                blockchain.add_block(pool.pool);
+
+                println!("{:?}", blockchain.find_unspent_transactions(&Pool::new_pool(), "Ivan"));
+            }
+        }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,10 @@
 use num_bigint::BigUint;
 use sha2::{Digest, Sha256};
 use std::time::{Duration, SystemTime};
-
 use serde::{Deserialize, Serialize};
-use serde_json::Result;
-
 use nut::{DBBuilder, DB};
+use std::collections::HashMap;
+
 
 #[cfg(test)]
 mod tests {
@@ -15,88 +14,204 @@ mod tests {
     }
 }
 
-const TARGET_BITS: u64 = 20;
+const TARGET_BITS: u64 = 12;
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct TXInput {
+    txid: BigUint,
+    vout_index: usize,
+    script_sig: String,
+}
+
+impl TXInput {
+    fn can_unlock_output_with(&self, unlock_data: &str) -> bool {
+        return self.script_sig == unlock_data;
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct TXOutput {
+    value: i64,
+    script_pubkey: String,
+}
+
+impl TXOutput {
+    fn can_be_unlocked_with(&self, unlock_data: &str) -> bool {
+        return self.script_pubkey == unlock_data;
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Transaction {
+    timestamp: Duration,
+    id: BigUint,
+    vin: Vec<TXInput>,
+    vout: Vec<TXOutput>,
+}
+
+impl Transaction {
+    pub fn new_coinbase_tx(to: String) -> Transaction {
+        let mut tx = Transaction {
+            timestamp: SystemTime::now()
+                .duration_since(SystemTime::UNIX_EPOCH)
+                .unwrap(),            
+            id: BigUint::new(vec![0u32; 8]),
+            vin: vec![TXInput {
+                txid: BigUint::new(vec![0u32; 8]),
+                vout_index: 0,
+                script_sig: String::from(format!("Reward to {}", to)),
+            }],
+            vout: vec![TXOutput {
+                value: 50,
+                script_pubkey: to,
+            }],
+        };
+
+        Transaction::set_id(&mut tx);
+        tx
+    }
+
+    fn serialize(temp: &Transaction) -> Vec<u8> {
+        serde_json::to_vec(temp).unwrap()
+    }    
+    
+    fn set_id(temp: &mut Transaction) {
+        let mut hasher = Sha256::new();
+        hasher.update(Transaction::serialize(temp));
+        //hasher.update();
+        let result = hasher.finalize();
+        temp.id = BigUint::from_bytes_le(result.as_slice());
+    }
+
+    fn is_coinbase(&self) -> bool {
+        return self.vin[0].txid == BigUint::new(vec![0u32; 8])
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Pool {
+    pub pool: Vec<Transaction>,
+}
+
+impl Pool {
+    pub fn new_pool() -> Pool {
+        Pool {
+            pool: Vec::new()
+        }
+    }
+
+    pub fn add_transaction_to_pool(&mut self, bc: &BlockChainDb, from: &str, to: &str, amount: i64) {
+
+        let (from_available, tx_table) = bc.find_spendable_transaction_output_index(self, from, amount);
+
+        if from_available < amount {
+            println!("{} not have enough money", from);
+            return
+        }
+
+        let mut inputs: Vec<TXInput> = Vec::new();
+        for (transaction_id, vout_index_collect) in tx_table {
+            for vout_index in vout_index_collect {
+                inputs.push(TXInput {
+                    txid: transaction_id.clone(),
+                    vout_index: vout_index,
+                    script_sig: String::from(from),
+                });
+            }
+        }
+
+        let mut outputs: Vec<TXOutput> = Vec::new();
+        outputs.push(TXOutput {
+            value: amount,
+            script_pubkey: String::from(to),
+        });
+
+        if from_available > amount {
+            outputs.push(TXOutput {
+                value: from_available - amount,
+                script_pubkey: String::from(from),
+            });            
+        }
+
+        let mut transaction = Transaction {
+            timestamp: SystemTime::now()
+                .duration_since(SystemTime::UNIX_EPOCH)
+                .unwrap(),            
+            id: BigUint::new(vec![0u32; 8]),
+            vin: inputs,
+            vout: outputs,
+        };
+
+        Transaction::set_id(&mut transaction);
+
+        self.pool.push(transaction);
+    }
+
+    pub fn add_coninbase_to_pool(&mut self, address: &str) {
+        self.pool.push(Transaction::new_coinbase_tx(String::from(address)));
+    }
+}
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Block {
     timestamp: Duration,
-    data: String,
+    transactions: Vec<Transaction>,
     prev_block_hash: BigUint,
     hash: BigUint,
     nonce: u128,
 }
 
-//sha2::digest::generic_array::GenericArray<u8, <Sha256 as Digest>::OutputSize>
+
 
 impl Block {
-    pub fn new_block(data: String, prev_block_hash: BigUint) -> Block {
-        let mut temp = Block {
+    pub fn new_genesis_block(coinbase: Transaction) -> Result<Block, ()> {
+
+        let mut block = Block {
             timestamp: SystemTime::now()
                 .duration_since(SystemTime::UNIX_EPOCH)
                 .unwrap(),
-            data: data,
-            prev_block_hash: prev_block_hash,
-            hash: BigUint::new(Vec::new()),
+            transactions: vec![coinbase],
+            prev_block_hash: BigUint::new(vec![0u32; 8]),
+            hash: BigUint::new(vec![0u32; 8]),
             nonce: 0,
         };
+        
+        match block.proof_of_work() {
+            Ok(_) => return Ok(block),
+            Err(_) => return Err(()),
+        }
+    }
 
-        temp.proof_of_work();
-        while !temp.validate_work() {
-            temp.timestamp = SystemTime::now()
+    pub fn new_block(transactions: Vec<Transaction>, prev_block_hash: BigUint) -> Result<Block, ()> {
+        let mut block = Block {
+            timestamp: SystemTime::now()
                 .duration_since(SystemTime::UNIX_EPOCH)
-                .unwrap();
-            temp.nonce = 0;
-            temp.proof_of_work();
-        }
-        temp
+                .unwrap(),
+            transactions: transactions,
+            prev_block_hash: prev_block_hash,
+            hash: BigUint::new(vec![0u32; 8]),
+            nonce: 0,
+        };
+        
+        match block.proof_of_work() {
+            Ok(_) => return Ok(block),
+            Err(_) => return Err(()),
+        }        
     }
 
-    fn validate_work(&mut self) -> bool {
-        let mut target_big = BigUint::new(Vec::new());
-        target_big.set_bit(256 - TARGET_BITS, true);
+    fn proof_of_work(&mut self) -> Result<(), ()> {
 
-        let mut hasher = Sha256::new();
-        hasher.update(
-            [
-                self.timestamp.as_nanos().to_string().as_bytes(),
-                self.data.as_bytes(),
-                &self.prev_block_hash.to_bytes_le()[..],
-                &TARGET_BITS.to_le_bytes(),
-                &self.nonce.to_le_bytes(),
-            ]
-            .concat(),
-        );
-        let result = hasher.finalize();
-
-        let temp = BigUint::from_bytes_le(result.as_slice());
-
-        if temp < target_big {
-            return true;
-        } else {
-            return false;
-        }
-    }
-    // fn set_hash(&mut self) {
-    //     let mut hasher = Sha256::new();
-    //     hasher.update( [self.timestamp.as_nanos().to_string().as_bytes(), self.data.as_bytes(), &self.prev_block_hash ].concat() );
-    //     let result = hasher.finalize();
-    //     self.hash = result.as_slice().try_into().expect("slice with incorrect length");
-    // }
-
-    fn proof_of_work(&mut self) {
         let mut hasher = Sha256::new();
         let mut result = hasher.finalize_reset();
 
-        //let v: Vec<u32> = Vec::new();
         let mut target_big = BigUint::new(Vec::new());
         target_big.set_bit(256 - TARGET_BITS, true);
-        //println!("target_big = {:064x}", target_big);
 
         while self.nonce < u128::MAX {
             hasher.update(
                 [
                     self.timestamp.as_nanos().to_string().as_bytes(),
-                    self.data.as_bytes(),
+                    &self.hash_transactions().to_bytes_le()[..],
                     &self.prev_block_hash.to_bytes_le()[..],
                     &TARGET_BITS.to_le_bytes(),
                     &self.nonce.to_le_bytes(),
@@ -111,11 +226,22 @@ impl Block {
             self.hash = BigUint::from_bytes_le(result.as_slice());
 
             if self.hash < target_big {
-                break;
+                return Ok(())
             } else {
                 self.nonce = self.nonce + 1;
             }
         }
+        return Err(())
+    }
+
+    fn hash_transactions(&self) -> BigUint {
+        let mut hasher = Sha256::new();
+        for i in &self.transactions {
+            hasher.update(&i.id.to_bytes_le()[..]);
+        }
+        let result = hasher.finalize();
+
+        BigUint::from_bytes_le(result.as_slice())
     }
 
     pub fn serialize(&self) -> Vec<u8> {
@@ -130,53 +256,19 @@ impl Block {
     pub fn show_block(i: &Block) {
         println!("TimeStamp: {:?}", i.timestamp);
         println!("Prev_hash: {:064x}", i.prev_block_hash);
-        println!("Data     : {}", i.data);
         println!("Hash     : {:064x}", i.hash);
         println!("Nonce    : {}", i.nonce);
-        println!("");
-    }    
-}
-
-#[derive(Debug)]
-pub struct BlockChain {
-    blocks: Vec<Block>,
-}
-
-impl BlockChain {
-    pub fn new_blockchain() -> BlockChain {
-        let genesis_block =
-            Block::new_block(String::from("Genesis Block"), BigUint::new(vec![0u32; 8]));
-        let mut blockchain = BlockChain { blocks: Vec::new() };
-        blockchain.blocks.push(genesis_block);
-        blockchain
-    }
-    pub fn add_block(&mut self, data: String) {
-        // let temp = Block::new_block(data, self.blocks[self.blocks.len() - 1].hash.clone());
-        // let j = serde_json::to_string(&temp).unwrap();
-        // println!("{}", j);
-        //self.blocks.push(temp);
-        self.blocks.push(Block::new_block(
-            data,
-            self.blocks[self.blocks.len() - 1].hash.clone(),
-        ))
-    }
-
-    pub fn show_blockchain(&self) {
-        for i in &self.blocks {
-            println!("TimeStamp: {:?}", i.timestamp);
-            println!("Prev_hash: {:064x}", i.prev_block_hash);
-            println!("Data     : {}", i.data);
-            println!("Hash     : {:064x}", i.hash);
-            println!("Nonce    : {}", i.nonce);
-            println!("");
+        for temp in &i.transactions {
+            println!("- Transaction: ");
+            //println!("  id:      {:064x}", temp.id);
+            println!("  timestamp: {:?}", temp.timestamp);
+            println!("  id:        {:?}", temp.id);
+            println!("  vin:       {:?}", temp.vin);
+            println!("  vout:      {:?}", temp.vout);
         }
-    }
+        println!("");
+    }      
 }
-
-pub fn test_work() {
-    println!("This is a test")
-}
-
 
 pub struct BlockChainDb {
     tip: Vec<u8>,
@@ -184,7 +276,7 @@ pub struct BlockChainDb {
 }
 
 impl BlockChainDb {
-    pub fn new_blockchain() -> BlockChainDb {
+    pub fn new_blockchain(address: String) -> BlockChainDb {
         let mut tip: Vec<u8> = Vec::new();
 
         let mut db = DBBuilder::new("test.db").build().unwrap();
@@ -204,7 +296,7 @@ impl BlockChainDb {
         }
 
         if flag == 1 {
-            let genesis_block = Block::new_block(String::from("Genesis Block"), BigUint::new(vec![0u32; 8]));
+            let genesis_block = Block::new_genesis_block(Transaction::new_coinbase_tx(address)).unwrap();
             let mut blocks = tx.create_bucket(b"blocksBucket").unwrap();
             blocks.put(
                 &genesis_block.hash.to_bytes_le(),
@@ -225,7 +317,7 @@ impl BlockChainDb {
         }
     }
 
-    pub fn add_block(&mut self, data: String) {
+    pub fn add_block(&mut self, transactions: Vec<Transaction>) {
         let last_hash;
         {
             let tx = self.db.begin_tx().unwrap();
@@ -233,7 +325,7 @@ impl BlockChainDb {
             last_hash = blocks.get(b"l").unwrap().to_vec();
         }
 
-        let new_block = Block::new_block(data, BigUint::from_bytes_le(&last_hash[..]));
+        let new_block = Block::new_block(transactions, BigUint::from_bytes_le(&last_hash[..])).unwrap();
 
         let mut tx = self.db.begin_rw_tx().unwrap();
         let mut blocks = tx.bucket_mut(b"blocksBucket").unwrap();
@@ -249,6 +341,148 @@ impl BlockChainDb {
         ).unwrap();    
 
         self.tip = new_block.hash.to_bytes_le();
+    }
+
+    pub fn show_blockchain(&self) {
+        let mut blockchainiterator = BlockchainIterator::new(&self);
+
+        loop {
+            if let Some(block) = blockchainiterator.next() {
+                //println!("{:?}", block)
+                Block::show_block(&block);
+            }
+            else {
+                break;
+            }
+        }     
+    }
+
+    pub fn find_spendable_transaction_output_index(&self, pool: &Pool, address: &str, amount: i64) -> (i64, HashMap<BigUint, Vec<usize>>) {
+
+        let mut sum: i64 = 0;
+        let mut spendable_table: HashMap<BigUint, Vec<usize>> = HashMap::new();
+        let mut flag: u8 = 0;
+
+        let transactions = self.find_unspent_transactions(pool, address);
+        for transaction in transactions {
+            for (output_index, output) in transaction.vout.iter().enumerate() {
+                if output.can_be_unlocked_with(address) && sum < amount {
+                    sum += output.value;
+                    spendable_table.entry(transaction.id.clone()).or_insert(Vec::new()).push(output_index);
+
+                    if sum >= amount {
+                        flag = 1;
+                        break;
+                    }
+                }
+            }
+            if flag == 1 {
+                flag = 0;
+                break;
+            }
+        }
+        (sum, spendable_table)
+    }
+
+    pub fn find_unspent_transactions(&self, pool: &Pool, address: &str) -> Vec<Transaction> {
+
+        let mut unspent_transactions: Vec<Transaction> = Vec::new();
+        let mut spent_table: HashMap<BigUint, Vec<usize>> = HashMap::new();
+
+        let mut flag: u8 = 0;
+        //pool
+        for transaction in pool.pool.iter().rev() {
+            for (output_index, output) in transaction.vout.iter().enumerate() {
+                if let Some(temp) = spent_table.get(&transaction.id) {
+                    for &i in temp {
+                        if i == output_index {
+                            flag = 1;
+                            break;
+                        }
+                    }
+                }
+
+                if flag == 1 {
+                    flag = 0;
+                    break;
+                }
+
+                if output.can_be_unlocked_with(address) {
+                    unspent_transactions.push(transaction.clone())
+                }
+            }
+
+            if transaction.is_coinbase() == false {
+                for input in &transaction.vin {
+                    if input.can_unlock_output_with(address) {
+                        spent_table.entry(input.txid.clone()).or_insert(Vec::new()).push(input.vout_index);
+                    }
+                }
+            }
+        }
+
+
+
+        //blockchain
+        flag = 0;
+
+        let mut blockchainiterator = BlockchainIterator::new(&self);
+
+        loop {
+            if let Some(block) = blockchainiterator.next() {
+                for transaction in block.transactions {
+                    for (output_index, output) in transaction.vout.iter().enumerate() {
+                        if let Some(temp) = spent_table.get(&transaction.id) {
+                            for &i in temp {
+                                if i == output_index {
+                                    flag = 1;
+                                    break;
+                                }
+                            }
+                        }
+        
+                        if flag == 1 {
+                            flag = 0;
+                            break;
+                        }
+        
+                        if output.can_be_unlocked_with(address) {
+                            unspent_transactions.push(transaction.clone())
+                        }
+                    }
+        
+                    if transaction.is_coinbase() == false {
+                        for input in &transaction.vin {
+                            if input.can_unlock_output_with(address) {
+                                spent_table.entry(input.txid.clone()).or_insert(Vec::new()).push(input.vout_index);
+                            }
+                        }
+                    }
+                }                
+            }
+            else {
+                break;
+            }
+        }             
+
+
+
+        unspent_transactions
+    }
+
+    pub fn get_balance(&self, address: &str) -> i64 {
+        let transactions = self.find_unspent_transactions(&Pool::new_pool(), address);
+        let mut sum: i64 = 0;
+
+        for transaction in transactions {
+            for temp in transaction.vout {
+                if temp.can_be_unlocked_with(address) {
+                    sum += temp.value;
+                }
+            }
+        }
+
+        sum
     }
 }
 
@@ -285,4 +519,3 @@ impl Iterator for BlockchainIterator {
         }
     }
 }
-
